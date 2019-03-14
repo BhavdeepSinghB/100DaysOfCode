2cb5efaa24c21868a0c9e1a8be6ecdb2f4e0ff2e
Bhavdeep Singh <bhavdeepsmail@gmail.com>
2019-03-13 17:26:33 -0700

    Day 3/1: Part one of 3/13/2019

--- Timer/Timer.py
+++ Timer/Timer.py
@@ -39,21 +39,7 @@ if(finalHr < incrementHr):
 print "{}:{}:{}".format(finalHr, finalMin, finalSec)
 
 while(incrementMin != finalMin) or (incrementSec != finalSec) or (incrementHr != finalHr):
-    
-    if finalMin - incrementMin < 0:
-        if finalSec - incrementSec <= 0:
-            print "%02d:%02d:%02d" % (finalHr - incrementHr - 1, 60 + (finalMin - incrementMin), 60 + (finalSec - incrementSec))
-        else:
-            print "%02d:%02d:%02d" % (finalHr - incrementHr, 60 + finalMin-incrementMin, finalSec - incrementSec)
-    else:
-        if finalSec - incrementSec < 0:   
-            print "%02d:%02d:%02d" % (finalHr - incrementHr - 1, 60 - finalMin-incrementMin + 1, 60 + (finalSec - incrementSec))
-        else:
-            print "%02d:%02d:%02d" % (finalHr - incrementHr, finalMin-incrementMin, finalSec - incrementSec)
-    
-    sys.stdout.write("\033[F")
-    time.sleep(1)
-    
+
     incrementSec += 1
     if incrementSec == 60:
         incrementMin += 1
@@ -61,6 +47,22 @@ while(incrementMin != finalMin) or (incrementSec != finalSec) or (incrementHr !=
     if incrementMin == 60:
         incrementHr += 1
         incrementMin = 0
+    
+    if finalMin - incrementMin < 0:
+        if finalSec - incrementSec < 0:
+            print "%02d:%02d:%02d" % (finalHr - incrementHr, (60 + finalMin) - incrementMin, (60 + finalSec) - incrementSec)
+        else:
+            print "%02d:%02d:%02d" % (finalHr - incrementHr, (60 + finalMin) - incrementMin, finalSec - incrementSec)
+    else:
+        if finalSec - incrementSec < 0: 
+            print "%02d:%02d:%02d" % (finalHr - incrementHr, (finalMin-incrementMin) - 1, (60 + finalSec) - incrementSec)
+        else:
+            print "%02d:%02d:%02d" % (finalHr - incrementHr, finalMin-incrementMin, finalSec - incrementSec)
+    
+    sys.stdout.write("\033[F")
+    time.sleep(1)
+    
+    
     
 print "{}:{}:{}".format(incrementHr,incrementMin, incrementSec)
 
