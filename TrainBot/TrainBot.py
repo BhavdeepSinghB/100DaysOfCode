1c159635d83b5b90ea3a32e7e5ba2244ca789bb7
Bhavdeep Singh <bhavdeepsmail@gmail.com>
2019-03-19 22:38:25 -0700

    Day 7: 3/19/2019

--- TrainBot/TrainBot.py
+++ TrainBot/TrainBot.py
@@ -1,25 +1,56 @@
 import asyncio
 import discord
 import time
+import collections
+from threading import Timer
 
-TOKEN = 'NTU3MzMzMTMyODY1Njk5ODQz.D3GxZg.EFfkSxAU3IHWmERPTzhbnX85CDQ'
+TOKEN = 'your token here'
+inviteCode = 'your invite here'
 
 client = discord.Client()
 
+
+trainQueue = collections.deque([])
+
+
+@client.event
+async def trainStuff():
+        if(len(trainQueue) != 0):
+            global popped
+            popped = trainQueue.popleft()
+            await popped.send("Your invite code is ready, please type !claim to claim it. You have 10 minutes")
+
 @client.event
 async def on_message(message):
-    if message.author == client.user:
+    #client.loop.create_task(trainStuff())
+    person = message.author
+    if person == client.user:
         return
 
     if message.content.lower().startswith('!train add'):
-        await message.channel.send("{0.name} added to the train".format(message.author))
-        addToStack(message.author)
 
-trainStack = []
+        if addToQueue(person) == True:
+            await message.channel.send("{0.name} added to the train".format(person))
+        else:
+            await message.channel.send("{0.name} is already on the train".format(person))
+    
+    await trainStuff()
 
-def addToStack(author):
-    trainStack.append(author)
-    print (trainStack)
+    if(message.content.lower().startswith('!claim')):
+        if(message.author == popped):
+            print("here")
+            await message.author.send("{} is your invite code, please generate an invite and use !invite + your code to keep the train moving".format(inviteCode))
+        else:
+            await message.author.send("Wait your turn")
+
+def addToQueue(author):
+    if author not in trainQueue:
+        trainQueue.append(author)
+        return True
+    return False
+
+
+    
 
 
 
