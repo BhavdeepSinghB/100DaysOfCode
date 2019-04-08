86f7a3e0ae404572c7e34612629362dd908b4ff1
Bhavdeep Singh <bhavdeepsmail@gmail.com>
2019-04-08 00:00:35 -0700

    Day 17: 4/7/2019

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 def index(request):
 	return HttpResponse("Hello World")
 
-def detail(request, questionid):
+def detail(request, questionid): 
 	return HttpResponse("You're looking at question {}".format(questionid))
 
 def results(request, questionid):
