cbdc832c0dcdd9dd200e8c6fd5b3b24b68d22fb4
Bhavdeep Singh <bhavdeepsmail@gmail.com>
2019-04-10 20:15:21 -0700

    Day 19: 4/10/2019

--- TextFileHash/TextFileHash.cpp
+++ TextFileHash/TextFileHash.cpp
@@ -4,32 +4,49 @@
 #include<cctype>
 using namespace std;
 
+void linearProbing() {
+    return;
+}
+
 string readFromFile(string fileName, int offset) {
     fstream f;
-    string returnable;
-    f.open("/Users/bhavdeep/Documents/100DaysOfCode/TextFileHash/" + fileName, ios::in);
-    if(f) {
+    string returnable = "";
+    f.open("/Users/Bhavdeep/Documents/100DaysOfCode/TextFileHash/" + fileName, ios::in);
     f.seekg(offset);
-        while (!f.eof()) {
-            char c = f.get();
+    if(f) {
+        f.seekg(offset);
+        char c = f.get();
+        returnable += c;
+        while (c != '%' && !f.eof()) {
+            c = f.get();
             if(c == EOF) {
                 continue;
             }
-            cout<<c;
+            returnable += c;
         }
         f.close();
-        return "something";
+        return returnable;
     }
-    return "something else";
+    return "NULL";
 }
 
 void writeToFile(string fileName, string content, int offset) {
     fstream f;
-    f.open("/Users/bhavdeep/Documents/100DaysOfCode/TextFileHash/" + fileName, ios::out);
-    f<<"Hello";
+    f.open("/Users/bhavdeep/Documents/100DaysOfCode/TextFileHash/" + fileName, ios::out | ios::in);
+    f.seekg(offset);
+
+    if(f) {
+        string fileContent = readFromFile(fileName, offset);
+        if(fileContent.empty() || fileContent[0] == EOF) {
+            //TODO: Figure out a way to read the content size with minimum code
+            //Comment - Might have to change the entire blueprint of the program
+        }
+
+    }
 }
 
+
 int main() {
-    //writeToFile("textMemory.txt", "hello", 3);
-    readFromFile("main.cpp", 0);
+    writeToFile("TextFileHashMemory.txt", "hello", 3);
+    cout<<readFromFile("TextFIleHashMemory.txt", 3);
 }
